f0779214336b29ac05c63f64201b28f12a6989ab
Marty Jones <murtin.jones@gmail.com>
2017-11-23 12:25:08 -0500

    slient error on import site failure v2

--- pypy/interpreter/app_main.py
+++ pypy/interpreter/app_main.py
@@ -586,7 +586,7 @@ def run_command_line(interactive,
         try:
             import site
         except:
-            print >> sys.stderr, "'import site' failed"
+            # print >> sys.stderr, "'import site' failed"
 
     set_stdio_encodings(ignore_environment)
 
